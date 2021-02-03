49c97626020aeab594aa71eadac7333dcbcb663a
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-02-02 17:47:29 -0700

    commit tree a88d9092784119f2bf6135f1e565e051c281d300

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+require 'pry'
+class CashRegister
 
+    attr_accessor :discount, :total, :quantity, :items, :values, :price
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+        @values = []
+    end
+    def add_item(title, price, quantity = 1)
+        @price = price
+        @total += (price * quantity)
+        @values << price
+        quantity.times do
+        @items << title
+        end
+    end
+
+    def apply_discount
+       if @discount != 0
+       self.total -= @total * @discount / 100
+       return "After the discount, the total comes to $#{self.total}."
+       else
+        return "There is no discount to apply."
+       end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        sum = 0
+        @values[0...-1].each do |items|
+            sum = sum + items
+        end
+        @total = sum
+    end
+end
+# binding.pry
